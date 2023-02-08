a5014b9079593cecbd390e7010adb415d4f4cf6d
luigiranalli96 <luigi.ranalli@univie.ac.at>
2023-02-08 14:40:58 +0100

    Update en_processing.py

--- vasp-phonopy-sscha/en_processing.py
+++ vasp-phonopy-sscha/en_processing.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
-POPULATION = 2
-
 def en_processing(pop_id=1):
     en_path = "vasp/energies"
     energies = pd.read_csv(en_path, engine='python', sep="  ",header=None)
     print(energies)
     energies = energies[3]*0.0734986176
-    processed_energy_path= f"data/energies_supercell_population{pop_id}.dat"
+    processed_energy_path= f"data/energies_supercell_population{pop_id[0][0]}.dat"
     e=open(processed_energy_path,"w+")
     energies.to_csv(e, index=False, header=False, float_format="%16.12f", sep='\t', mode="w+")
     e.close()
