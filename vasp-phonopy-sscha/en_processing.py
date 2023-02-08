7980895a70c002193a9096e0c1326baa101db8a4
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-08 11:29:20 +0100

    Fix bug

--- vasp-phonopy-sscha/en_processing.py
+++ vasp-phonopy-sscha/en_processing.py
@@ -7,7 +7,7 @@ def en_processing(pop_id=1):
     energies = pd.read_csv(en_path, engine='python', sep="  ",header=None)
     print(energies)
     energies = energies[3]*0.0734986176
-    processed_energy_path= f"data/energies_supercell_population{pop_id}.dat"
+    processed_energy_path= f"data/energies_supercell_population{pop_id[0][0]}.dat"
     e=open(processed_energy_path,"w+")
     energies.to_csv(e, index=False, header=False, float_format="%16.12f", sep='\t', mode="w+")
     e.close()
