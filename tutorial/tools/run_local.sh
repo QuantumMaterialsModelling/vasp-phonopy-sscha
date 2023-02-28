15d3dd35fa6a2fbb9b80a66d6b902cb147e03cee
diegomartinez2 <diegomartinez2@yahoo.es>
2023-02-28 10:43:51 +0100

    Fix bug in forces output

--- tutorial/tools/run_local.sh
+++ tutorial/tools/run_local.sh
@@ -121,7 +121,7 @@ for i in `seq 1 $NCONFSSCHA`; do
 #    mpirun -np $np vasp_std > stdout
     ~/VASP/vasp.6.3.0/bin/vasp_std > stdout
     grep "energy  without entropy" OUTCAR  >> energies
-    grep "forces" -A $IONS vasprun.xml > forces/forces_population$POPULATION'_'$i.dat
+    grep "forces" -A $IONS vasprun.xml > forces/forces_population$(($POPULATION+1))'_'$i.dat
     rm POSCAR
     mv OUTCAR OUTCAR_$i
     echo `date` >> timing
